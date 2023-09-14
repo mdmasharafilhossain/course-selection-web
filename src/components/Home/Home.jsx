04da28d7cc1842b8467aa8c1cef8b0b6d8000cf8
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-09-14 22:32:02 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-center mt-20">Course Registration</h1>
+
+        </div>
+    );
+};
+
+export default Home;
